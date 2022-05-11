a22f1155faec20134e700bcd967d6a8716868940
Daanii Kusnanta <danynabilgk@gmail.com>
2022-05-11 13:42:37 +0700

    update book controller for book detail

--- src/main/kotlin/com/raassh/quiz2pbkk22/Quiz2PBKK22/controller/BookController.kt
+++ src/main/kotlin/com/raassh/quiz2pbkk22/Quiz2PBKK22/controller/BookController.kt
@@ -1,14 +1,15 @@
 package com.raassh.quiz2pbkk22.Quiz2PBKK22.controller
 
+import com.raassh.quiz2pbkk22.Quiz2PBKK22.form.ReviewForm
 import com.raassh.quiz2pbkk22.Quiz2PBKK22.repository.BookRepository
 import com.raassh.quiz2pbkk22.Quiz2PBKK22.utils.Views
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
-
 
 @Controller
 @RequestMapping("/books")
@@ -34,5 +35,26 @@ class BookController {
         }
 
         return Views.BOOK_INDEX
+    }
+
+    @GetMapping("/{id}")
+    fun findBook(
+        @PathVariable id: Long,
+        model: Model
+    ): String {
+        try {
+            bookRepository.findById(id).ifPresentOrElse({
+                val book = it
+                model.addAttribute("book", book)
+            }) {
+                model.addAttribute("error", "Book Not Found")
+            }
+
+            model.addAttribute("reviewForm", ReviewForm())
+        } catch (e: java.lang.Exception) {
+            model.addAttribute("error", e.message)
+        }
+
+        return Views.BOOK_DETAIL
     }
 }
